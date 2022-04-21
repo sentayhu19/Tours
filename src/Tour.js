8b1e8a7e6cd7b1dba3178e7f9863483c90c52992
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-04-21 23:18:39 +0300

    Not intersted btn added and other css stylings

--- src/Tour.js
+++ src/Tour.js
@@ -2,10 +2,11 @@ import React from "react";
 import './Tour.css';
 const Tour =({id,image,info,price,name})=>{
     return( <div className="tour-wrap">
-        <p className="name">{name}</p>
+        <p className="name"><b>{name}</b></p>
         {<img src={image} className="img" />}
         <p className="info">{info}</p>
         <p className="price">$ {price}</p>
+        <button className="not">Not intersted</button>
         </div>
         );
 }

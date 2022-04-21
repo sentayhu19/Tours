463af1ddfe0e01c0e59d3fd59e88f967d1ea6bfd
Sentayhu berhanu <sentayhuberhanu19+github@gmail.com>
2022-04-21 22:20:59 +0300

    styling on load  added

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,12 @@
 import React from "react";
 import './Tour.css';
 const Tour =({id,image,info,price,name})=>{
-    return( <div><p>{name}</p>{<img src={image} />}{info}{price}</div>);
+    return( <div className="tour-wrap">
+        <p className="name">{name}</p>
+        {<img src={image} className="img" />}
+        <p>{info}</p>
+        <p>$ {price}</p>
+        </div>
+        );
 }
 export default Tour;
